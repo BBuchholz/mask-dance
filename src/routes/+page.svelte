24092da0dc500c0f4e161bccd5c57db65e090326
Brent Buchholz <brentbuchholz@gmail.com>
2023-02-17 11:39:25 -0600

    Add buttons and functions in preparations for wiring up Supabase

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,55 @@
+<script>
+// @ts-nocheck
+
+    import Button from './Button.svelte'
+
+    const defaultText = 'Click Me'
+    let responseText = defaultText
+
+    function handleClick(messageAppension){
+        //TODO: move this into a svelte notification? Just 
+        //      for future reference, like boardCE does
+        alert("Just testing from the scaffolding, Clicked " + messageAppension)
+    }
+
+    async function handleClickHelloWorld(){
+        const response = 
+            await fetch('/.netlify/functions/hello-world')
+		        .then(response => response.json()
+	        )
+
+        console.log(response)
+        responseText = response.message
+    }
+
+    function handleClickReset(){
+        responseText = defaultText
+    }
+
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+
+
+<Button class="primary sm" on:click={() => handleClick('one')}>
+	{responseText}
+</Button>
+
+<Button class="danger lg" on:click={() => handleClick('two')}>
+	{responseText}
+</Button>
+
+
+
+<Button on:click={() => handleClickHelloWorld()}>
+    {responseText}
+</Button>
+
+{#if responseText !== defaultText}
+<Button on:click={() => handleClickReset()}>
+    Reset
+</Button>
+{/if}
---
+++ src/routes/Button.svelte
@@ -0,0 +1,32 @@
+<script>
+// @ts-nocheck
+
+	
+	let buttonProps = {
+	class:[$$restProps.class]
+	}
+</script>
+	<!-- svelte-ignore a11y-mouse-events-have-key-events -->
+	<button on:click
+			on:mouseover
+			on:mouseenter
+			on:mouseleave
+		{...buttonProps}>
+			<slot/>
+	</button>
+
+<style>
+	.primary{
+		color:green;
+	}
+	.danger {
+		color:red;
+	}
+	.sm {
+		font-size:1em;
+		padding:0.1em;
+	}
+	.lg {
+		font-size:2em
+	}
+</style>
